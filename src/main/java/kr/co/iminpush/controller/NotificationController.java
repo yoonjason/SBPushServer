2215a1a53d8a284f0d6bb76ea08261b3b26dafcb
ysyoon <luffy1021@naver.copm>
2020-10-28 14:38:56 +0900

    * jsp 연결 및 테스트 완료

--- src/main/java/kr/co/iminpush/controller/NotificationController.java
+++ src/main/java/kr/co/iminpush/controller/NotificationController.java
@@ -12,9 +12,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.CompletableFuture;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class NotificationController {
@@ -24,19 +31,47 @@ public class NotificationController {
     @Autowired
     PushNotificationService pushNotificationService;
 
-//    @RequestMapping(value = "/")
-//    public @ResponseBody String Main2(){
-//        return "index.html";
+    @RequestMapping(value = "/")
+    public String main() {
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
+
+        return "test";
+    }
+
+//    @RequestMapping(value = "/notification")
+//    public String notfication() {
+//        return "notification.html";
 //    }
 
-    @RequestMapping(value = "/notification")
-    public String main(){
-        return "notification.html";
+//    @RequestMapping(value = "/test")
+//    public String contentView(Model model) {
+//        model.addAttribute("value", "hi");
+//        return "test.html";
+//    }
+
+
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception{
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "TEST");
+
+        List<String> testList = new ArrayList<String>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+        System.out.println("/jsp잘 타는지?");
+        mav.addObject("list", testList);
+        return mav;
     }
 
 
     @GetMapping(value = "/send")
-    public @ResponseBody ResponseEntity<String> send() throws JSONException, InterruptedException  {
+    public @ResponseBody
+    ResponseEntity<String> send() throws JSONException, InterruptedException {
         String notifications = PushPeriodicNotifications.PeriodicNotificationJson();
 
         HttpEntity<String> request = new HttpEntity<>(notifications);
@@ -44,15 +79,13 @@ public class NotificationController {
         CompletableFuture<String> pushNotification = pushNotificationService.send(request);
         CompletableFuture.allOf(pushNotification).join();
 
-        try{
+        try {
             String firebaseResponse = pushNotification.get();
             return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
-        }
-        catch (InterruptedException e){
+        } catch (InterruptedException e) {
             logger.debug("got interrupted!");
             throw new InterruptedException();
-        }
-        catch (ExecutionException e){
+        } catch (ExecutionException e) {
             logger.debug("execution error!");
         }
 
